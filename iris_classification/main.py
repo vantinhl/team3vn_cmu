48c564c54a982a03d2ea0467ace96b6077b97b0f
vantinhl <vantinhl@andrew.cmu.edu>
2023-07-21 11:05:48 +0700

    ok

--- iris_classification/main.py
+++
@@ -1,9 +0,0 @@
-from src.train import Classifier
-
-cls = Classifier()
-
-def lambda_handler(event, context):
-    try:
-        return cls.load_and_test(event)
-    except Exception as e:
-        raise
