a28e7c47f9eae6eb89c6f77904d6c19eadd95653
vantinhl <138684392+vantinhl@users.noreply.github.com>
2023-07-21 11:18:50 +0700

    Update model.py

--- iris_classification/model.py
+++ iris_classification/model.py
@@ -8,7 +8,7 @@ import joblib
 seed = 42
 
 # Read original dataset
-iris_df = pd.read_csv("data/iris.csv")
+iris_df = pd.read_csv("iris_classification/data/iris.csv")
 iris_df.sample(frac=1, random_state=seed)
 
 # selecting features and target data
