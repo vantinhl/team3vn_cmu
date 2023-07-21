55da8595e5cfc1796dd7dd4762c898a9586fbd79
vantinhl <138684392+vantinhl@users.noreply.github.com>
2023-07-21 19:15:48 +0700

    Update prediction.py

--- diabetes-classification/prediction.py
+++ diabetes-classification/prediction.py
@@ -2,9 +2,5 @@ import joblib
 
 
 def predict(data):
-<<<<<<< HEAD
     clf = joblib.load("rf_model.sav")
-=======
-    clf = joblib.load("./model/rf_model.sav")
->>>>>>> 3f5ebd334f486c0583671cbee93bc0a7ded9998b
     return clf.predict(data)
